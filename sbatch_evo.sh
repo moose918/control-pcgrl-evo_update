0e6060dd4e2764486a3eca35454099ea2904363b
Musa Gumpu <gumpu.musa@gmail.com>
2023-10-20 19:23:23 +0200

    setup script

--- sbatch_evo.sh
+++ sbatch_evo.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-#SBATCH --nodes=1
-##SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=12
-
-#SBATCH --partition=stampede
-#SBATCH --time=72:00:00
-
-#SBATCH --job-name=evo_batch
-
-#SBATCH --output=/home-mscluster/mgumpu/control-pcgrl-evo_update/logs/out.log
-#SBATCH --error=/home-mscluster/mgumpu/control-pcgrl-evo_update/logs/err.log
-
-
-source /home-mscluster/mgumpu/.bashrc
-cd /home-mscluster/mgumpu/control-pcgrl-evo_update || exit
-
-# We try to activate the relevant conda environment below.
-conda activate pcgrl
 
 sbatch sbatch_aux_nca.sh & sbatch sbatch_nca.sh & sbatch sbatch_cppn.sh
