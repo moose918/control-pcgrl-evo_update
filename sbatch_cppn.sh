a4b4174630491041e01c10c0d660d9d92306a8b3
Musa <gumpu.musa@gmail.com>
2023-11-03 07:08:25 +0200

    local setup

--- sbatch_cppn.sh
+++ sbatch_cppn.sh
@@ -11,8 +11,8 @@
 #SBATCH --output=/home-mscluster/mgumpu/control-pcgrl-evo_update/logs/cppn_out.log
 #SBATCH --error=/home-mscluster/mgumpu/control-pcgrl-evo_update/logs/cppn_err.log
 
-export RAY_LOG_TO_STDERR=1
-export GLOG_logtostderr=1
+#export RAY_LOG_TO_STDERR=1
+#export GLOG_logtostderr=1
 source /home-mscluster/mgumpu/.bashrc
 cd /home-mscluster/mgumpu/control-pcgrl-evo_update || exit
 cp ./configs/evo/batch_CPPN.yaml ./configs/evo/batch.yaml
@@ -20,10 +20,10 @@ cp ./configs/evo/batch_CPPN.yaml ./configs/evo/batch.yaml
 # We try to activate the relevant conda environment below.
 conda activate pcgrl
 
-#python run_batch_evo.py --local || exit
-#python run_batch_evo.py --local --evaluate || exit
-#python run_batch_evo.py --local --cross-eval || exit
+python run_batch_evo.py --local || exit
+python run_batch_evo.py --local --evaluate || exit
+python run_batch_evo.py --local --cross-eval || exit
 
-python run_batch_evo.py || exit
-python run_batch_evo.py --evaluate || exit
-python run_batch_evo.py --cross-eval || exit
+#python run_batch_evo.py || exit
+#python run_batch_evo.py --evaluate || exit
+#python run_batch_evo.py --cross-eval || exit
