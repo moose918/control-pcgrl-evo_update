6de62c165c1adc809ca902cdbe305e8b2836d16c
Musa <gumpu.musa@gmail.com>
2023-11-03 07:04:56 +0200

    SLURM setup

--- sbatch_aux_nca.sh
+++ sbatch_aux_nca.sh
@@ -20,10 +20,10 @@ cp ./configs/evo/batch_AuxNCA.yaml ./configs/evo/batch.yaml
 # We try to activate the relevant conda environment below.
 conda activate pcgrl
 
-python run_batch_evo.py --local || exit
-python run_batch_evo.py --local --evaluate || exit
-python run_batch_evo.py --local --cross-eval || exit
+#python run_batch_evo.py --local || exit
+#python run_batch_evo.py --local --evaluate || exit
+#python run_batch_evo.py --local --cross-eval || exit
 
-#python run_batch_evo.py || exit
-#python run_batch_evo.py --evaluate || exit
-#python run_batch_evo.py --cross-eval || exit
+python run_batch_evo.py || exit
+python run_batch_evo.py --evaluate || exit
+python run_batch_evo.py --cross-eval || exit
