749760a074449b448ea3d13d5a0590e4eccd60e2
MusaMSS <mng@mscluster>
2023-11-16 12:14:49 +0200

    Eval setup

--- sbatch_eval_nca.sh
+++ sbatch_eval_nca.sh
@@ -6,10 +6,10 @@
 #SBATCH --partition=bigbatch
 #SBATCH --time=72:00:00
 
-#SBATCH --job-name=nca_batch
+#SBATCH --job-name=eval_nca_batch
 
-#SBATCH --output=/home-mscluster/mgumpu/control-pcgrl-evo_update/logs/nca_out.log
-#SBATCH --error=/home-mscluster/mgumpu/control-pcgrl-evo_update/logs/nca_err.log
+#SBATCH --output=/home-mscluster/mgumpu/control-pcgrl-evo_update/logs/eval_nca_out.log
+#SBATCH --error=/home-mscluster/mgumpu/control-pcgrl-evo_update/logs/eval_nca_err.log
 
 #export RAY_LOG_TO_STDERR=1
 #export GLOG_logtostderr=1
@@ -20,9 +20,9 @@ cp ./configs/evo/batch_NCA.yaml ./configs/evo/batch.yaml
 # We try to activate the relevant conda environment below.
 conda activate pcgrl
 
-python run_batch_evo.py --local || exit
 python run_batch_evo.py --local --evaluate || exit
-python run_batch_evo.py --local --cross-eval || exit
+python run_batch_evo.py --local -ce || exit
+python evo_batch.py --local --gif -tex || exit
 
 #python run_batch_evo.py || exit
 #python run_batch_evo.py --evaluate || exit
--- sbatch_evo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sbatch sbatch_nca.sh &
-sleep 10
-sbatch sbatch_aux_nca.sh & 
-sleep 10
-sbatch sbatch_cppn.sh
